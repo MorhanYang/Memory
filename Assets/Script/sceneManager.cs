d88f8daa062cd02173bdc789f32c0207dbde44c8
PrisicllaX <grapexuputian@126.com>
2022-12-07 13:20:15 -0500

    DialogueFunction

--- Assets/Script/sceneManager.cs
+++ Assets/Script/sceneManager.cs
@@ -9,6 +9,6 @@ public class sceneManager : MonoBehaviour,IPointerClickHandler
     public void OnPointerClick(PointerEventData eventData)
     {
         Debug.Log("clicked!");
-        SceneManager.LoadScene("bar");
+        SceneManager.LoadScene(1);
     }
 }
