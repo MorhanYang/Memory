09e0ce2d4273f8e97eba9a21cef98e508e060035
PrisicllaX <grapexuputian@126.com>
2022-12-12 03:16:44 -0500

    Opening Scene Animation half finished

--- Assets/lightBlink.cs
+++ Assets/lightBlink.cs
@@ -5,21 +5,35 @@ using UnityEngine;
 public class lightBlink : MonoBehaviour
 {
     // Start is called before the first frame update
+    public int BlinkChanceInt;
     private bool flag;
+
+    private int a;
 
     void Start()
     {
-        int a = Random.Range(0, 4);
+        a = Random.Range(0, 10);
+        flag=true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        a = Random.Range(0, 10);
+        if (flag&a<=BlinkChanceInt)
+        {
+            StartCoroutine(Blink());
+        }
         
     }
-    void blink()
-    {
-       // GetComponent<SpriteRenderer>().enable= true;
 
+    IEnumerator Blink()
+    {
+        flag =false; 
+         GetComponent<SpriteRenderer>().enabled= true;
+         yield return new WaitForSeconds(a/2.0f);
+         GetComponent<SpriteRenderer>().enabled= false; 
+         flag =true;   
     }
+
 }
--- Assets/showSlow.cs
+++ Assets/showSlow.cs
@@ -6,7 +6,7 @@ public class showSlow : MonoBehaviour
 {
     // Start is called before the first frame update
     private float co;
-    public float changingSpeed=0.01f;
+    public float changingSpeed=0.005f;
     private float timer;
     void Start()
     {
